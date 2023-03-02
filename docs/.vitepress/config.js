f146366775a10b686a0b0aa88847f86c8139252f
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2023-03-02 14:42:23 -0500

    advacned demo

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,15 @@
 export default {
-    title: 'Anon Shop Blog',
-    description: 'A Blog About Anon Shop'
+  themeConfig: {
+    nav: [
+      { text: 'Guide', link: '/guide' },
+      {
+        text: 'Dropdown Menu',
+        items: [
+          { text: 'Item A', link: '/item-1' },
+          { text: 'Item B', link: '/item-2' },
+          { text: 'Item C', link: '/item-3' }
+        ]
+      }
+    ]
   }
+}
